640f9441fc04b1d7cbda6e758c3ab68a87965204
Liam Teale <liamteale6@gmail.com>
2025-03-03 11:21:35 -0800

    readd competition C API

--- src/devices/competition.cpp
+++ src/devices/competition.cpp
@@ -12,33 +12,62 @@
  */
 
 #include "kapi.h"
+#include "v5_api.h"
+
+// TODO: remove this C API
+extern "C" {
+uint8_t competition_get_status(void) {
+    return vexCompetitionStatus();
+}
+
+uint8_t competition_is_disabled(void) {
+    return (competition_get_status() & COMPETITION_DISABLED) != 0;
+}
+
+uint8_t competition_is_connected(void) {
+    return (competition_get_status() & COMPETITION_CONNECTED) != 0;
+}
+
+uint8_t competition_is_autonomous(void) {
+    return (competition_get_status() & COMPETITION_AUTONOMOUS) != 0;
+}
+
+uint8_t competition_is_field(void) {
+    return ((competition_get_status() & COMPETITION_SYSTEM) != 0) && competition_is_connected();
+}
+
+uint8_t competition_is_switch(void) {
+    return ((competition_get_status() & COMPETITION_SYSTEM) == 0) && competition_is_connected();
+}
+}
 
 namespace pros {
 namespace competition {
 using namespace pros::c;
+
 std::uint8_t get_status(void) {
-	return competition_get_status();
+    return competition_get_status();
 }
 
 std::uint8_t is_autonomous(void) {
-	return competition_is_autonomous();
+    return competition_is_autonomous();
 }
 
 std::uint8_t is_connected(void) {
-	return competition_is_connected();
+    return competition_is_connected();
 }
 
 std::uint8_t is_disabled(void) {
-	return competition_is_disabled();
+    return competition_is_disabled();
 }
 
 std::uint8_t is_field_control(void) {
-	return competition_is_field();
+    return competition_is_field();
 }
 
 std::uint8_t is_competition_switch(void) {
-	return competition_is_switch();
+    return competition_is_switch();
 }
 
-}  // namespace competition
-}  // namespace pros
+} // namespace competition
+} // namespace pros
