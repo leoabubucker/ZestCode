a5ce94639221ea4967d1918d321261723ffb615f
Leo Abubucker <leokumar@outlook.com>
2025-03-03 16:36:26 -0500

    move html files to root

---
+++ .d/main.cpp.d
@@ -0,0 +1,23 @@
+bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
+ include/pros/colors.h include/pros/error.h include/pros/llemu.h \
+ include/pros/misc.h include/pros/rtos.h include/pros/colors.hpp \
+ include/pros/llemu.hpp include/pros/misc.hpp include/pros/misc.h \
+ include/pros/rtos.hpp include/pros/rtos.h include/pros/screen.hpp \
+ include/pros/screen.h include/pros/colors.h include/pros/colors.hpp
+include/main.h:
+include/api.h:
+include/pros/colors.h:
+include/pros/error.h:
+include/pros/llemu.h:
+include/pros/misc.h:
+include/pros/rtos.h:
+include/pros/colors.hpp:
+include/pros/llemu.hpp:
+include/pros/misc.hpp:
+include/pros/misc.h:
+include/pros/rtos.hpp:
+include/pros/rtos.h:
+include/pros/screen.hpp:
+include/pros/screen.h:
+include/pros/colors.h:
+include/pros/colors.hpp:
