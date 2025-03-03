ee552b6776e911f0d13faeae24c8f3d2e29dc38c
leoabubucker <leokumar@outlook.com>
2025-03-03 21:50:20 +0000

    Deploying to gh-pages from @ leoabubucker/ZestCode@1c10007415b56fba12938a76a560e961a220b941 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -10,11 +10,12 @@ var searchData=
   ['cobs_5fencode_5fmeasure_7',['cobs_encode_measure',['../cobs_8h.html#ada10fe2699a8483601dec03862960806',1,'cobs.h']]],
   ['colors_20c_20api_8',['Colors C API',['../group__c-colors.html',1,'']]],
   ['colors_2eh_9',['colors.h',['../colors_8h.html',1,'']]],
-  ['competition_5fget_5fstatus_10',['competition_get_status',['../group__c-misc.html#ga71a09bfc4dbfb9c07c917c701dc3e994',1,'misc.h']]],
-  ['competition_5fis_5fautonomous_11',['competition_is_autonomous',['../group__c-misc.html#gabefe28014a85f9c430468c6ca944dd5a',1,'misc.h']]],
-  ['competition_5fis_5fconnected_12',['competition_is_connected',['../group__c-misc.html#ga048efc863026021d9710441065ecb359',1,'misc.h']]],
-  ['competition_5fis_5fdisabled_13',['competition_is_disabled',['../group__c-misc.html#ga61c606d7f3c52e88d2e1a192fce950b0',1,'misc.h']]],
-  ['competition_5fis_5ffield_14',['competition_is_field',['../group__c-misc.html#gab32495bd9fafc0ee5238704ff95817eb',1,'misc.h']]],
-  ['contributing_15',['Contributing',['../index.html#autotoc_md4',1,'']]],
-  ['current_5ftask_16',['CURRENT_TASK',['../group__c-rtos.html#gad028cb03c935557184a71a6c53915065',1,'rtos.h']]]
+  ['commit_10',['ZestCode test commit',['../index.html',1,'']]],
+  ['competition_5fget_5fstatus_11',['competition_get_status',['../group__c-misc.html#ga71a09bfc4dbfb9c07c917c701dc3e994',1,'misc.h']]],
+  ['competition_5fis_5fautonomous_12',['competition_is_autonomous',['../group__c-misc.html#gabefe28014a85f9c430468c6ca944dd5a',1,'misc.h']]],
+  ['competition_5fis_5fconnected_13',['competition_is_connected',['../group__c-misc.html#ga048efc863026021d9710441065ecb359',1,'misc.h']]],
+  ['competition_5fis_5fdisabled_14',['competition_is_disabled',['../group__c-misc.html#ga61c606d7f3c52e88d2e1a192fce950b0',1,'misc.h']]],
+  ['competition_5fis_5ffield_15',['competition_is_field',['../group__c-misc.html#gab32495bd9fafc0ee5238704ff95817eb',1,'misc.h']]],
+  ['contributing_16',['Contributing',['../index.html#autotoc_md4',1,'']]],
+  ['current_5ftask_17',['CURRENT_TASK',['../group__c-rtos.html#gad028cb03c935557184a71a6c53915065',1,'rtos.h']]]
 ];
