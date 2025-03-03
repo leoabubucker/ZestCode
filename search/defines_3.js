5667f8b70b237784a2f36015dc6bb036197bf6d9
Leo Abubucker <leokumar@outlook.com>
2025-03-03 16:53:09 -0500

    remove unnecessary files from gh-pages

--- search/defines_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['return_5fport_0',['return_port',['../vdml_8h.html#ae6bf38331fdc0366cc0f0ab04ced5926',1,'vdml.h']]]
-];
--- search/files_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['banners_2eh_0',['banners.h',['../banners_8h.html',1,'']]]
-];
