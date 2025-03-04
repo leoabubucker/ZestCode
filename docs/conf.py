07cd557c0f9a619ce4ff1a056032cef64e1699e0
Leo Abubucker <leokumar@outlook.com>
2025-03-04 06:40:38 -0500

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,16 @@
-import os
-import subprocess
+import subprocess, os
 
-# Run Doxygen to generate XML before building Sphinx docs
-subprocess.call('doxygen', shell=True)
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
-# Sphinx extensions
-extensions = ["breathe"]
+if read_the_docs_build:
 
-# Breathe configuration
-breathe_projects = {"ZestCode": "xml"}
-breathe_default_project = "ZestCode"
+    # subprocess.call('cd ../doxygen; doxygen', shell=True)
+    # Run Doxygen to generate XML before building Sphinx docs
+    subprocess.call('doxygen', shell=True)
+
+    # Sphinx extensions
+    extensions = ["breathe"]
+
+    # Breathe configuration
+    breathe_projects = {"ZestCode": "xml"}
+    breathe_default_project = "ZestCode"
