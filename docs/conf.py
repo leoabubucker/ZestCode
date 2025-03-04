0d5696ad3db22ec1dbe0379f8a2cb78051f19101
Leo Abubucker <leokumar@outlook.com>
2025-03-04 06:53:47 -0500

    manually add index.rst file

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,11 @@
 import subprocess, os
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+# Run Doxygen to generate XML before building Sphinx docs
+subprocess.call('doxygen', shell=True)
 
-if read_the_docs_build:
+# Sphinx extensions
+extensions = ["breathe"]
 
-    # subprocess.call('cd ../doxygen; doxygen', shell=True)
-    # Run Doxygen to generate XML before building Sphinx docs
-    subprocess.call('doxygen', shell=True)
-
-    # Sphinx extensions
-    extensions = ["breathe"]
-
-    # Breathe configuration
-    breathe_projects = {"ZestCode": "xml"}
-    breathe_default_project = "ZestCode"
+# Breathe configuration
+breathe_projects = {"ZestCode": "xml"}
+breathe_default_project = "ZestCode"
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+.. ZestCode documentation master file
+
+Welcome to ZestCode's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api_reference  # This will link to Doxygen-generated API docs
+
+API Reference
+=============
+
+.. doxygenindex::
