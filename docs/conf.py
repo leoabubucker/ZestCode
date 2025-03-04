77d4466a3b87e6d1a1623a139e9ab960b6a83eaf
Leo Abubucker <leokumar@outlook.com>
2025-03-04 06:34:15 -0500

    update doc creation

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,12 @@
-import subprocess, os
+import os
+import subprocess
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+# Run Doxygen to generate XML before building Sphinx docs
+subprocess.call('doxygen', shell=True)
 
-if read_the_docs_build:
+# Sphinx extensions
+extensions = ["breathe"]
 
-     subprocess.call('doxygen', shell=True)
+# Breathe configuration
+breathe_projects = {"ZestCode": "xml"}
+breathe_default_project = "ZestCode"
